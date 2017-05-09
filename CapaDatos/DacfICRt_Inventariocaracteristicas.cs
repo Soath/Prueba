aad915f5c067b90904c3ba5894662b88e7f3a9e3
Soath <darielch1000@gmail.com>
2017-05-09 10:20:17 -0500

    Efectuar inventario

--- CapaDatos/DacfICRt_Inventariocaracteristicas.cs
+++ CapaDatos/DacfICRt_Inventariocaracteristicas.cs
@@ -1504,5 +1504,39 @@ namespace CapaDatos
         return DtResultado;
     }
 
+    public string Copiar()
+    {
+
+        string rpta = "";
+        SqlConnection SqlCon = new SqlConnection();
+        try
+        {
+            //Código
+            SqlCon.ConnectionString = DConexion.CnBDActivo;
+            SqlCon.Open();
+            //Establecer el Comando
+            SqlCommand SqlCmd = new SqlCommand();
+            SqlCmd.Connection = SqlCon;
+            SqlCmd.CommandText = "usp_C_acfICRt_Inventariocaracteristicas";
+            SqlCmd.CommandType = CommandType.StoredProcedure;
+            //
+
+
+            //Ejecutamos nuestro comando
+
+            rpta = SqlCmd.ExecuteNonQuery() != 0 ? "OK" : "NO se Elimino el Registro";
+
+
+        }
+        catch (Exception ex)
+        {
+            rpta = ex.Message;
+        }
+        finally
+        {
+            if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+        }
+        return rpta;
+    }
 }
 }
