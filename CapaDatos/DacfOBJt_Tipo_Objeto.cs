0229d01a8680cc1b9eb692b2a32d4cac11cda0cb
Soath <darielch1000@gmail.com>
2017-06-14 11:56:24 -0500

    Datos y negocio tipo objeto

---
+++ CapaDatos/DacfOBJt_Tipo_Objeto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DacfOBJt_Tipo_Objeto
+    {
+
+
+        public DataTable Mostrar()
+        {
+            string rpta = "";
+            DataTable DtResultado = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+
+
+
+            try
+            {
+                //Codigo
+                SqlCon.ConnectionString = DConexion.CnBDActivo;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCon.Open();
+                SqlCmd.CommandText = "SELECT OBJid_objeto,OBJdescripcion,OBJorden FROM acfOBJt_Tipo_Objeto";
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+                rpta = SqlCmd.ExecuteNonQuery() != 0 ? "OK" : "NO se Actualizo el Registro";
+            }
+
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+                DtResultado = null;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return DtResultado;
+        }
+
+
+    }
+
+
+}
+
---
+++ CapaNegocio/NacfOBJt_Tipo_Objeto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class NacfOBJt_Tipo_Objeto
+    {
+
+
+        public static DataTable Mostrar()
+        {
+            return new DacfOBJt_Tipo_Objeto().Mostrar();
+        }
+    }
+}
