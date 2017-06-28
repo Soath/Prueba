c342b225a99489691fc6380b11fa8deb4f94b6ea
Soath <darielch1000@gmail.com>
2017-06-28 10:45:19 -0500

    Mapeo tablas 2

---
+++ CapaNegocio/NACFVR_IDENT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class NACFVR_IDENT
+    {
+
+
+        public static DataTable Mostrar()
+        {
+            return new DACFVR_IDENT().Mostrar();
+        }
+    }
+}
