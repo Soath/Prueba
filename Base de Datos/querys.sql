ce15d9ec68ed0dce813dc5b5c74d75bc65a622f4
Samael <sing_sang_x1'00pre@hotmail.com>
2017-07-13 11:25:14 -0500

    commit luego del merge

--- Base de Datos/querys.sql
+++ Base de Datos/querys.sql
@@ -5,3 +5,5 @@ alter column [UBEid] int null
 
 Alter table [dbo].[acfINBt_Inventariobienes] 
 Add default '0' for [ACFvutiltribanio] 
+
+EXECUTE sp_rename 'Sales.SalesTerritory.TerritoryID', 'TerrID', 'COLUMN';
