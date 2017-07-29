ecf5607f33634991efc118b361e75edcc56cf36f
Samael <sing_sang_x1'00pre@hotmail.com>
2017-07-29 06:25:09 -0500

    Frm GIS Procesar -- botones

--- CapaPresentacion/FrmacfGIS_UbicacionElectricaProcesar.Designer.cs
+++ CapaPresentacion/FrmacfGIS_UbicacionElectricaProcesar.Designer.cs
@@ -632,6 +632,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(765, 493);
+            this.ControlBox = false;
             this.Controls.Add(this.label32);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox28);
