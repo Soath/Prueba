14edd6e93fca1a3e33e229b6d920fff7ef074521
Samael <sing_sang_x1'00pre@hotmail.com>
2017-07-29 06:23:30 -0500

    Frm GIS Faltante

--- CapaPresentacion/FrmacfGIS_UbicacionElectricaImportar.Designer.cs
+++ CapaPresentacion/FrmacfGIS_UbicacionElectricaImportar.Designer.cs
@@ -107,7 +107,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.ClientSize = new System.Drawing.Size(648, 598);
+            this.ControlBox = false;
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
